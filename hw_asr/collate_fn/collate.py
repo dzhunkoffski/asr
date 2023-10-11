61e6752cba9a964dd98b86e74f884fb42c129d7f
Kirill <dzhunkofffski@gmail.com>
2023-10-11 21:38:23 +0300

    added segment length

--- hw_asr/collate_fn/collate.py
+++ hw_asr/collate_fn/collate.py
@@ -15,9 +15,11 @@ def collate_fn(dataset_items: List[dict]):
     text_batch = []
     text_encoded_batch = []
     text_encoded_length_batch = []
+    spectrogram_length_batch = []
     max_spectrogram_len = 0
     for item in dataset_items:
         audio_batch.append(item['audio'])
+        spectrogram_length_batch.append(item['spectrogram'].size()[-1])
         max_spectrogram_len = max(item['spectrogram'].size()[-1], max_spectrogram_len)
         duration_batch.append(item['duration'])
         text_batch.append(item['text'])
@@ -32,6 +34,7 @@ def collate_fn(dataset_items: List[dict]):
     spectrogram_batch = torch.stack(spectrogram_batch)
     text_encoded_batch = torch.nn.utils.rnn.pad_sequence(text_encoded_batch, batch_first=True, padding_value=PADDING_VALUE).int()
     text_encoded_length_batch = torch.tensor(text_encoded_length_batch).int()
+    spectrogram_length_batch = torch.tensor(spectrogram_length_batch).int()
     return {
         'audio': audio_batch,
         'spectrogram': spectrogram_batch,
