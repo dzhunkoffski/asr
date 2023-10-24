1df32b7c0089cac42749228fc5f1ab0436ff0016
Kirill <dzhunkofffski@gmail.com>
2023-10-24 10:52:35 +0300

    Final run

--- hw_asr/text_encoder/char_text_encoder.py
+++ hw_asr/text_encoder/char_text_encoder.py
@@ -57,7 +57,8 @@ class CharTextEncoder(BaseTextEncoder):
             self.tokenizer = spm.SentencePieceProcessor(model_file=tokenizer_config['model_path'] + '.model')
             return  [self.tokenizer.id_to_piece(i) for i in range(self.tokenizer.get_piece_size())]
         else:
-            self._prepare_training_texts(tokenizer_config['train_datasets'])
+            if tokenizer_config['train_text_path'] is None:
+                self._prepare_training_texts(tokenizer_config['train_datasets'])
             spm.SentencePieceTrainer.train(f'--input=data/datasets/train.txt --model_prefix={tokenizer_config["model_path"]} --vocab_size={tokenizer_config["vocab_size"]} --model_type=bpe --unk_id=0 --bos_id=-1 --eos_id=-1')
             self.tokenizer = spm.SentencePieceProcessor()
             self.tokenizer.load(tokenizer_config['model_path'] + '.model')
